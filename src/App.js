eba6377750bddd0faae3f98b1e676e5a9bab7c58
Jonathan Nocek <jnocek@iwu.edu>
2020-08-20 18:56:48 -0500

    Add info box to each pin

--- src/App.js
+++ src/App.js
@@ -1,51 +1,71 @@
-import React from 'react'
-import { GoogleMap, 
-  useLoadScript, 
+/*
+ * Created using this tutorial: https://www.youtube.com/watch?v=WZcxJGmLbSo
+ */
+
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
   Marker,
-  InfoWindow
-} from "@react-google-maps/api"; 
+  InfoWindow,
+} from "@react-google-maps/api";
 import usePlacesAutocomplete, {
   getGeocode,
-  getLatLng
+  getLatLng,
 } from "use-places-autocomplete";
 import {
   Combobox,
   ComboboxInput,
   ComboboxPopover,
   ComboboxList,
-  ComboboxOption
+  ComboboxOption,
 } from "@reach/combobox";
-import "@reach/combobox/styles.css"
-import mapStyles from './mapStyles';
-import "./index.css"
-require('dotenv').config()
-
+import "@reach/combobox/styles.css";
+import mapStyles from "./mapStyles";
+import "./index.css";
+require("dotenv").config();
 
 // Constants
-const libraries = ["places"]
+const libraries = ["places"];
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh'
-}
+  width: "100vw",
+  height: "100vh",
+};
 // Center of the map (Currently: Chicago)
 const center = {
   lat: 41.8781,
   lng: -87.6298,
-}
+};
 // Styles from snazzy maps
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
-}
-
+};
 
 function App() {
-  const {isLoaded, loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
   const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
   if (loadError) return "Error Loading Maps";
   if (!isLoaded) return "Loading Maps";
@@ -53,26 +73,18 @@ function App() {
   return (
     <div>
       <h1>
-        Golf Courses{" "} 
+        Golf Courses{" "}
         <span role="img" aria-label="golf-hole">
           ⛳
         </span>
       </h1>
-      <GoogleMap 
-        mapContainerStyle={mapContainerStyle} 
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
         zoom={8}
         center={center}
         options={options}
-        onClick={(event) => {
-          setMarkers(current => [
-            ...current, 
-            {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng(),
-              time: new Date(),
-            },
-          ]);
-        }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
         {markers.map((marker) => (
           <Marker
@@ -80,17 +92,32 @@ function App() {
             position={{ lat: marker.lat, lng: marker.lng }}
             icon={{
               url: "/pin.png",
-              scaledSize: new window.google.maps.Size(30,30),
-              origin: new window.google.maps.Point(0,0),
-              anchor: new window.google.maps.Point(15,30),
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 30),
+            }}
+            onClick={() => {
+              setSelected(marker);
             }}
           />
-        ))};
+        ))}
+        ;
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Golf Course Name</h2>
+              <p>Golf Course Rating</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   );
 }
-
-
 
 export default App;
