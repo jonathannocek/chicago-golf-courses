8831aa175f155b46d0bc8a45aa5f8f3d614b0e12
Jonathan Nocek <jnocek@iwu.edu>
2020-08-22 10:26:53 -0500

    Add courses that will populate on the load of the map

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import mapStyles from "./mapStyles";
 import Search from "./Search";
 import Locate from "./Locate";
 import "./index.css";
+import * as golfCourseData from "./data/golfCourseData.json";
 require("dotenv").config();
 
 // Constants
@@ -25,8 +26,8 @@ const mapContainerStyle = {
 };
 // Center of the map (Currently: Chicago)
 const center = {
-  lat: 41.8781,
-  lng: -87.6298,
+  lat: golfCourseData.cities.Chicago.lat,
+  lng: golfCourseData.cities.Chicago.lng,
 };
 // Styles from snazzy maps
 const options = {
@@ -59,10 +60,10 @@ function App() {
     mapRef.current = map;
   }, []);
 
-  const panTo = React.useCallback(({lat, lng}) => {
-    mapRef.current.panTo({lat, lng});
-    mapRef.current.setZoom(14)
-}, []);
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
 
   if (loadError) return "Error Loading Maps";
   if (!isLoaded) return "Loading Maps";
@@ -76,8 +77,8 @@ function App() {
         </span>
       </h1>
 
-      <SearchBox panTo={panTo}/>
-      <LocateButton panTo={panTo}/>
+      <SearchBox panTo={panTo} />
+      <LocateButton panTo={panTo} />
 
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
@@ -87,10 +88,13 @@ function App() {
         onClick={onMapClick}
         onLoad={onMapLoad}
       >
-        {markers.map((marker) => (
+        {golfCourseData.courses.map((golfCourse) => (
           <Marker
-            key={marker.time.toISOString()}
-            position={{ lat: marker.lat, lng: marker.lng }}
+            key={golfCourse.id}
+            position={{
+              lat: golfCourse.lat,
+              lng: golfCourse.lng,
+            }}
             icon={{
               url: "/pin.png",
               scaledSize: new window.google.maps.Size(30, 30),
@@ -98,7 +102,7 @@ function App() {
               anchor: new window.google.maps.Point(15, 30),
             }}
             onClick={() => {
-              setSelected(marker);
+              setSelected(golfCourse);
             }}
           />
         ))}
@@ -111,8 +115,7 @@ function App() {
             }}
           >
             <div>
-              <h2>Golf Course Name</h2>
-              <p>Golf Course Rating</p>
+              <h4>{selected.name}</h4>
             </div>
           </InfoWindow>
         ) : null}
@@ -121,12 +124,12 @@ function App() {
   );
 }
 
-function SearchBox({panTo}) {
-    return Search({panTo});
+function SearchBox({ panTo }) {
+  return Search({ panTo });
 }
 
-function LocateButton({panTo}) {
-  return Locate({panTo});
+function LocateButton({ panTo }) {
+  return Locate({ panTo });
 }
 
 export default App;
